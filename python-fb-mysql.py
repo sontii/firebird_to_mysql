59f2137c362ea158aca84e4bee3a16d86d4f2772
sontii <sontii78@gmail.com>
2022-07-01 13:09:12 +0200

    logging + date difference

--- python-fb-mysql.py
+++ python-fb-mysql.py
@@ -2,48 +2,73 @@ from ast import arg
 import sys
 import datetime
 from datetime import date, timedelta
+import logging
+from attr import s
 import fdb
 import mysql.connector
 from mysql.connector import Error
 
-def validate(date_text):
+logging.basicConfig(filename="logfile.log", encoding='utf-8', level=logging.INFO)
+
+
+""" bolti lekérés
+"select sum(bteny_ert), sum(nteny_ert), sum(nyilv_ert) from blokk_tet where datum = '" & Format(tegnapt, "yyyy.MM.dd") & "'"
+-- """
+
+""" központ lekérés
+"select sum(bteny_ert), sum(nteny_ert), sum(nyilv_ert) from blokk_tet where datum = '" & Format(tegnapt, "yyyy.MM.dd") & "' and egyseg='" & boltkod & "'
+-- """
+
+
+""" check argv date is valid  """
+def validate(date_text):    
         try:
             datetime.datetime.strptime(date_text, '%Y.%m.%d')
         except ValueError:
-            raise ValueError("dátum formátum: YYYY.MM.DD")
+            logging.error(" " + datetime.datetime.now().strftime('%Y.%m.%d %H:%M:%S') + " dátum formátum: YYYY.MM.DD")
+            exit(1)
 
 def main():
-
+    havingARGV = False
     yesterday = date.today() - timedelta(days=1)
 
+    """ if we have argv """
     if len(sys.argv) == 3:
         startDate = sys.argv[1]
         endDate = sys.argv[2]
         validate(startDate)
         validate(endDate)
+        havingARGV = True
+        if startDate > endDate:
+            logging.error(" " + datetime.datetime.now().strftime('%Y.%m.%d %H:%M:%S') + " A kezdő dátum nem lehet nagyobb mint a vég dátum")
+            exit(1)
     else:
+
         startDate = yesterday.strftime("%Y.%m.%d")
         endDate = startDate
 
 
-    """ firebird  """
+    """ firebird read """
     try:
         con = fdb.connect(
             host='192.168.103.51', database='D:\Program Files\Laurel Kft\AIR Application\database\ibukr.gdb',
             user='SYSDBA', password='masterkey' 
         )
         cur = con.cursor()
-        SELECT = "select * from AFA"
+        if havingARGV == False:
+            SELECT = "SELECT SUM(bteny_ert), SUM(nteny_ert), SUM(nyilv_ert) FROM blokk_tet WHERE datum = '%s' AND egyseg = '001'" % endDate
+        else:
+            SELECT = f"SELECT SUM(bteny_ert), SUM(nteny_ert), SUM(nyilv_ert) from blokk_tet where datum '{startDate}' between '{endDate}' and egyseg = 001"
         cur.execute(SELECT)
-        """ for row in cur:
-            print(row) """
+        for row in cur:
+            print(row)
 
     except Error as e:
-        print("Error while connecting to Firebird-SQL", e)
+        logging.error(" " + datetime.datetime.now().strftime('%Y.%m.%d %H:%M:%S') + " Error while connecting to Firebird-SQL", e)
+        exit(1)
 
 
-    """ mysql """
-
+    """ mysql write"""
     try:
         connection = mysql.connector.connect(host='192.168.103.101',
                                             database='lazar',
@@ -57,15 +82,14 @@ def main():
         """ for row in records:
             print(row) """
         
-            
-
     except Error as e:
-        print("Error while connecting to MySQL", e)
+        logging.error(" " + datetime.datetime.now().strftime('%Y.%m.%d %H:%M:%S') + " Error while connecting to MySQL", e)
+        exit(1)
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
-            print("MySQL connection is closed")
+            logging.info(" " + datetime.datetime.now().strftime('%Y.%m.%d %H:%M:%S') + " MySQL connection is closed")
 
 if __name__ == "__main__":
     main()
