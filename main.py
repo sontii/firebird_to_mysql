5a14be72a0736f0fec776255435c5cd657b0e285
Fridrich Ferenc <sontii78@gmail.com>
2022-09-11 17:40:59 +0200

    refactor mysql and csv writer

--- main.py
+++ main.py
@@ -1,38 +1,22 @@
-from mysql_scripts.csv_to_my_aru import *
-from fb_scipts.get_lastaru_fb import *
-from fb_scipts.get_forgalom import *
-from fb_scipts.get_aru import *
-from mysql_scripts.get_lastaru_my import *
-from fb_scipts.fb_query import *
 import sys
 import datetime
 from datetime import date, timedelta
 import logging
 from dotenv import load_dotenv
 
+from query_fb import *
+from query_mysql import *
+
 load_dotenv()
 
 logging.basicConfig(filename="log/logfile.log",
                     encoding='utf-8', level=logging.INFO)
-
-# enviroment variables setup
-fbHost = os.getenv("FBHOST")
-fbData = os.getenv("FBDATA")
-fbUser = os.getenv("FBUSER")
-fbPass = os.getenv("FBPASS")
-
-mysqlHost = os.getenv("MYSQLHOST")
-mysqlData = os.getenv("MYSQLDATA")
-mysqlUser = os.getenv("MYSQLUSER")
-mysqlPass = os.getenv("MYSQLPASS")
 
 envBoltok = os.getenv("BOLTOK")
 
 boltok = []
 for bolt in envBoltok.split(","):
     boltok.append(bolt)
-
-selectLastAruId= ("SELECT FIRST 1 ID FROM CIK ORDER BY ID DESC")
 
 # check argv date is valid
 
@@ -65,17 +49,26 @@ def main():
         startDate = yesterday.strftime("%Y.%m.%d")
         endDate = startDate
 
-    lastIdFb = fbQuery(fbHost, fbData, fbUser, fbPass, selectLastAruId)
-    lastIdMysql = getLastaruMysql(mysqlHost, mysqlData, mysqlUser, mysqlPass)
+    # query variables
+    getLastIdFb = ("SELECT FIRST 1 ID FROM CIK ORDER BY ID DESC")
+    getLastIdMysql = ("SELECT max(id) FROM cikk")
 
-    getAru(lastIdFb, lastIdMysql)
+    # QUERYS:
+    # get last id for aru (query script, boltok, fetchType)
+    lastIdFb = int(queryFb(getLastIdFb, boltok, "one"))
+    lastIdMysql = int(queryMysql(getLastIdMysql, boltok, "one"))
 
-# TODO
-### getLastIDs - done
-### getAru (mysqlLastItem, fbLastItem)
-# aruToMysql(mysqlHost, mysqlData, mysqlUser, mysqlPass) csv -> to  mysql
-##getForgalom (startDate, endDate)
-### forgalomToMysql ()
+    if lastIdFb != lastIdMysql:
+        print('1')
+        # get range of aru
+        #getAru(lastIdFb, lastIdMysql)
+
+    # TODO
+    ### getLastIDs - done
+    ### getAru (mysqlLastItem, fbLastItem)
+    # aruToMysql(mysqlHost, mysqlData, mysqlUser, mysqlPass) csv -> to  mysql
+    ##getForgalom (startDate, endDate)
+    ### forgalomToMysql ()
 
 
 if __name__ == "__main__":
