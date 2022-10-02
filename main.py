30266aa881920df8228edac32f8dddc7a42b6a9e
sontii <sontii78@gmail.com>
2022-10-02 15:25:52 +0200

    forgalom fix

--- main.py
+++ main.py
@@ -194,9 +194,7 @@ def main():
 
             clearCsv("csv/tiltas.csv")
  
-    exit()
-    # TODO
-    if lastForgalomFb != lastForgalomMysql:
+    if lastForgalomMaxFb != lastForgalomMysql:
         ### blokk tetel forgalom
         getQuery = """ SELECT TRX_ID, EGYSEG, PT_GEP, DATUM, SORSZAM, ARUKOD, MENNY, ME, AFA_KOD, AFA_SZAZ, NYILV_AR, NYILV_ERT,
                         BFOGY_AR, BFOGY_ERT, NFOGY_ERT, BTENY_AR, BTENY_ERT, NTENY_ERT, NENG_ERT, BENG_ERT, TVR_AZON
